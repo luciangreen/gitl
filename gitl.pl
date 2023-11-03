2cece3a4809c5a07c134240fa0fd925b701f2b07
Lucian Green <luciangreen@lucianacademy.com>
2023-11-03 20:28:28 +1100

    Doesn't commit if folders are identical

--- gitl.pl
+++ gitl.pl
@@ -42,51 +42,95 @@ commit(Repository1) :-
  (N=0,number_string(N,NS),save_file_s(N_path,NS))),
 
  N1 is N+1,
- (number_string(N1,N1S),save_file_s(N_path,N1S)),
-
-
-  foldr(string_concat,[Gitl_data_path1,Repository,"/",N1,"/"],R2),
- (exists_directory_s(R2)->true;make_directory(R2)),
-
- %Gitl_data_path=R2,
  
-
-
- Scp="scp -pr ",
- foldr(string_concat,[Repository_root_path,Repository,"/*"],From),
- 
- 
- foldr(string_concat,[Gitl_data_path1,Repository,"/",N1,"/."],To),
- foldr(string_concat,[Scp,From," ",To],Command),
- 
- directory_files(R1,F),
-	delete_invisibles_etc(F,G),
-%trace,	
- (G=[]->true;
- shell1_s(Command)),
-
-
  (N=0->N0=N1;N0=N),
  foldr(string_concat,[Gitl_data_path1,Repository,"/",N0,"/"],To_m_1),
  foldr(string_concat,[Gitl_data_path1,Repository,"/",N1,"/"],To1),
 
  %trace,
 
- save_diff(To_m_1,To1,HTML),
-  foldr(string_concat,[Gitl_data_path1,Repository,"/",N,".html"],HTMLP),
+ %(N0=0->
+ 
+ 
+ 
+ 
+ ((To=To1,
+ find_files(R1,RTests),
+ (N1=1->(scp1(Repository_root_path,Repository,Gitl_data_path1,N1,R1,N_path),
+
+ HTML=(-));
+ (sd1(RTests,R1,To_m_1,Repository_root_path,Repository,Gitl_data_path1,N1,R1,N_path,To,HTML)))
+ 
+)
+ ->(
+
+  %working_directory1(_,A1),
+
+ 
+
+ %Gitl_data_path=R2,
+ 
+
+
+
+
+
+  foldr(string_concat,[Gitl_data_path1,Repository,"/",N1,".html"],HTMLP),
 
  working_directory1(_,A1),
- save_file_s(HTMLP,HTML).
+ (HTML=(-) ->true;
+ save_file_s(HTMLP,HTML))
+ );(writeln("Not committed. Folders identical."),abort)),!.
  
 %open_string_file_s
 
-save_diff(To_m_1,To,HTML3) :-
+%save_diff(N0,R1,To_m_1,To,HTML3) :-
+%trace,
+
+% .
+
+scp1(Repository_root_path,Repository,Gitl_data_path1,N1,R1,N_path) :-
+
+ (number_string(N1,N1S),save_file_s(N_path,N1S)),
+
+%trace,
+  foldr(string_concat,[Gitl_data_path1,Repository,"/",N1,"/"],R2),
+ (exists_directory_s(R2)->true;make_directory(R2)),
+
+
+ Scp="scp -pr ",
+ foldr(string_concat,[Repository_root_path,Repository,"/*"],From),
+ 
+ 
+ foldr(string_concat,[Gitl_data_path1,Repository,"/",N1,"/."],To_a),
+ foldr(string_concat,[Scp,From," ",To_a],Command),
+ 
+ directory_files(R1,F),
+	delete_invisibles_etc(F,G),
+%trace,	
+ (G=[]->true;
+ shell1_s(Command)).
+
+sd1(RTests,R1,To_m_1,Repository_root_path,Repository,Gitl_data_path1,N1,R1,N_path,To,HTML) :-
+
+%trace,
+ 
+ %findall(T1a,(member([T1,_],RTests),string_concat(R1,T1a,T1)),R11),
+ findall([T1a,BA],(member([T1,BA],RTests),string_concat(R1,T1a,T1)),R110),
 
  find_files(To_m_1,Tests1),
- find_files(To,Tests2),
- %trace,
  findall(T1a,(member([T1,_],Tests1),string_concat(To_m_1,T1a,T1)),T11),
+ findall([T1a,BA],(member([T1,BA],Tests1),string_concat(To_m_1,T1a,T1)),T110),
+ not(T110=R110),
+ 
+
+
+ scp1(Repository_root_path,Repository,Gitl_data_path1,N1,R1,N_path), 
+ 
+ find_files(To,Tests2),
  findall(T2b,(member([T2,_],Tests2),string_concat(To,T2b,T2)),T21),
+
+ %trace,
 
  intersection(T11,T21,IT12),
  subtract(T11,T21,D),
@@ -102,7 +146,7 @@ save_diff(To_m_1,To,HTML3) :-
  numbers(IT11L,1,[],Ns),
  
 
- findall([*,T1a,C],(member(N,Ns),get_item_n(IT11,N,[A1,B1]),string_concat(To_m_1,T1a,A1),get_item_n(IT123,N,[_A2,B2]),
+ findall([*,T1a,C],(member(NA,Ns),get_item_n(IT11,NA,[A1,B1]),string_concat(To_m_1,T1a,A1),get_item_n(IT123,NA,[_A2,B2]),
  split_string(B1,"\n\r","\n\r",IT111),
  split_string(B2,"\n\r","\n\r",IT121),
  %trace,
@@ -120,12 +164,9 @@ save_diff(To_m_1,To,HTML3) :-
  
  foldr(append,[IT11a,IT123a,CA],C1),
  
- diff(C1,HTML3)
- 
- 
-
- .
- 
+ diff(C1,HTML).
+  
+   
 diff(Before,After,After3) :-
  find_insertions_and_deletions_vintage(Before,After,Insertions,Deletions),
  replace11_vintage(After,Insertions,[],After2),
